9a8b9230956bd19a3d81fde6a2a8d30998639f21
Lyrise <lyrise1984@gmail.com>
2024-01-11 19:43:35 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use clap::Parser;
 use std::error::Error;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::time::Duration;
 use tokio::net::UdpSocket;
 use tokio::sync::mpsc;
+use tokio::time::sleep;
 
 #[derive(Parser)]
 struct Opts {
@@ -21,11 +25,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let (tx, mut rx) = mpsc::channel(100);
 
+    let sent_bytes = Arc::new(AtomicUsize::new(0));
+    let received_bytes = Arc::new(AtomicUsize::new(0));
+
+    let sent_bytes_clone = Arc::clone(&sent_bytes);
+    let received_bytes_clone = Arc::clone(&received_bytes);
+
+    tokio::spawn(async move {
+        loop {
+            sleep(Duration::from_secs(10)).await;
+            let sent = sent_bytes_clone.swap(0, Ordering::Relaxed);
+            let received = received_bytes_clone.swap(0, Ordering::Relaxed);
+            println!(
+                "Sent: {} bytes/sec, Received: {} bytes/sec",
+                sent / 10,
+                received / 10
+            );
+        }
+    });
+
     tokio::spawn(async move {
         loop {
             let mut buf = vec![0; 65535];
             match recv_socket.recv_from(&mut buf).await {
                 Ok((amt, _)) => {
+                    received_bytes.fetch_add(amt, Ordering::Relaxed);
                     if let Err(e) = tx.send(buf[..amt].to_vec()).await {
                         eprintln!("Failed to send packet: {}", e);
                     }
@@ -46,6 +70,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             {
                 Ok(sent) => {
                     total_sent += sent;
+                    sent_bytes.fetch_add(sent, Ordering::Relaxed);
                 }
                 Err(e) => {
                     eprintln!("Failed to send packet: {}", e);
